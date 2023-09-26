4ebb1a8d16403d5ba30ea696dc4dcf6bc74118fc
Marouane Bouaricha <marouane@itchallenge.fr>
2023-09-26 16:31:25 +0100

    removed detect_manager function

--- install_pipx.sh
+++ install_pipx.sh
@@ -1,23 +1,5 @@
 #!/usr/bin/env bash
 
-function detect_package_manager()
-{
-  declare -A osInfo;
-  osInfo[/etc/redhat-release]=yum
-  osInfo[/etc/debian_version]=apt-get
-  osInfo[/etc/SuSE-release]=zypper
-  osInfo[/etc/arch-release]=pacman
-  osInfo[/etc/gentoo-release]=emerge
-  osInfo[/etc/alpine-release]=apk
-
-  for f in ${!osInfo[@]}
-  do
-      if [[ -f $f ]];then
-          echo ${osInfo[$f]}
-      fi
-  done
-}
-
 function install_python()
 {   
     PYDIR=$HOME/opt/python-3.10.6
@@ -57,7 +39,7 @@ function install_python()
 # Check the distribution name and version
 DISTRO=$(cat /etc/os-release | grep -w NAME | cut -d= -f2 | tr -d '"')
 FULL_VERSION=$(cat /etc/os-release | grep -w VERSION_ID | cut -d= -f2 | tr -d '"')
-ubuntu_version=$(grep -oP '(?<=VERSION_ID=")[0-9]+' /etc/os-release)
+
 
 #Check package manager
 PKGM=$(detect_package_manager)
@@ -67,6 +49,7 @@ PKGM=$(detect_package_manager)
 case $DISTRO in
   Ubuntu)
     echo "Detected Ubuntu distribution"
+    ubuntu_version=$(grep -oP '(?<=VERSION_ID=")[0-9]+' /etc/os-release)
     if [ "$ubuntu_version" != "18" ] && [ "$ubuntu_version" != "20" ] && [ "$ubuntu_version" != "22" ]; then
         install_python
     else
