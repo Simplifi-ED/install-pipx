b816219954c37fe851b31680e1d682184ed5a904
Marouane Bouaricha <marouane@itchallenge.fr>
2023-09-26 16:27:03 +0100

    Added a custom python installation

--- install_pipx.sh
+++ install_pipx.sh
@@ -18,6 +18,41 @@ function detect_package_manager()
   done
 }
 
+function install_python()
+{   
+    PYDIR=$HOME/opt/python-3.10.6
+    export PATH=$PYDIR/bin:$PATH
+    export CPPFLAGS="-I$PYDIR/include $CPPFLAGS"
+
+    mkdir -p $PYDIR/src
+    cd $PYDIR/src
+
+    # openssl
+    wget https://www.openssl.org/source/openssl-1.1.1l.tar.gz
+    tar zxf openssl-1.1.1l.tar.gz
+    cd openssl-1.1.1l
+    ./config --prefix=$PYDIR
+    make
+    make install
+
+    # python
+    wget https://www.python.org/ftp/python/3.10.6/Python-3.10.6.tar.xz
+    tar xf Python-3.10.6.tar.xz
+    cd Python-3.10.6
+    ./configure --prefix=$PYDIR
+    make
+    make install
+    cd Python-3.10.6
+    sudo ./configure --enable-optimizations --prefix=$PYDIR
+    sudo make install
+    sudo update-alternatives --install /usr/bin/python3 python3 /usr/local/bin/python3.10 0
+    #sudo ln -sf /usr/bin/python3.10 /usr/bin/python3 
+    python3 -V
+
+    cd /opt
+    sudo rm -f Python-3.10.12.tgz
+}
+
 
 # Check the distribution name and version
 DISTRO=$(cat /etc/os-release | grep -w NAME | cut -d= -f2 | tr -d '"')
@@ -30,29 +65,40 @@ PKGM=$(detect_package_manager)
 
 # Install the packages according to the distribution
 case $DISTRO in
-  Ubuntu|Debian)
+  Ubuntu)
+    echo "Detected Ubuntu distribution"
+    if [ "$ubuntu_version" != "18" ] && [ "$ubuntu_version" != "20" ] && [ "$ubuntu_version" != "22" ]; then
+        install_python
+    else
+        # Update the package lists
+        sudo apt update -y
+        sudo apt install python3-pip -y
+        sudo apt install python3-venv -y
+        fi
+    ;;
+  Debian)
     echo "Detected Debian-based distribution"
     # Update the package lists
     sudo apt update
     sudo apt install python3-pip -y
     sudo apt install python3-venv -y
     ;;
-  Fedora|CentOS|RedHat)
+  Fedora|CentOS|"CentOS Linux"|RedHat)
     echo "Detected RedHat-based distribution"
     # Check if yum is available, and use it if available
     if command -v yum &> /dev/null; then
-        sudo yum update
+        sudo yum update -y
         sudo yum install -y python3-pip
     else
         # Use dnf if yum is not available
-        sudo dnf update
+        sudo dnf update -y
         sudo dnf install -y python3-pip
     fi
     ;;
   SLES)
-  echo "Detected RedHat-based distribution"
+  echo "Detected SUSE-based distribution"
     # Update the package lists
-    sudo zypper update
+    sudo zypper update -y
     # Install the packages
     sudo zypper install -y python3 python3-pip
     ;;
